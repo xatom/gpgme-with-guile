4cf1fbc35a9ff0a6916e091bcea09e54bc08bb2b
Atom X <atomx@deadlyhead.com>
2011-10-03 01:39:55 -0700

    Added hash algorithm handling
    This completes the algorithm identification functionality
    provided by GPGME.

--- src/gpg/algorithms.scm
+++ src/gpg/algorithms.scm
@@ -77,16 +77,18 @@
     (gpg:pk-ecdh  . "ECDH")))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Public key accessor functions ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Public key accessor functions
 
 ;; Translate public key symbol -> string
 (define (gpg:pubkey-algo-name algo)
   "\
 Returns a string containing a description of the public key algorithm
 @var{algo}.  This string can be used to output the name of the public
-key algorithm to the user."
+key algorithm to the user.
+
+If @var{algo} is not a recognized public key algorithm, returns
+@code{#f}."
   (assq-ref *gpg:public-key->name* algo))
 
 
@@ -94,8 +96,11 @@ key algorithm to the user."
 (define (gpg:pubkey-algo->integer algo)
   "\
 Translates the public key algorithm symbol @var{algo} to an integer;
-this function should only be used if interfacing to GPGME directly."
-  (assq-ref *gpg:public-key->num* algo))
+this function should only be used if interfacing to GPGME directly.
+
+If @var{algo} is not a recognized public key algorithm, returns
+@var{#f}."
+  (assq-ref *gpg:public-key->integer* algo))
 
 
 ;; Translate integer -> public key symbol (for interfacing to GPGME)
@@ -103,7 +108,93 @@ this function should only be used if interfacing to GPGME directly."
   "\
 Translates the integer @var{num} to its corresponding public key
 algorithm symbol; this function should only be used if interfacing
-to GPGME directly."
+to GPGME directly.
+
+If @var{num} is not a recognized value, returns @var{#f}."
   (assq-ref *gpg:public-key-algorithm-enum* num))
- 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;;; Hash algorithms ;;;
+;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Data structures
+
+;; A direct translation of gpgme_hash_algo_t -> alist
+;; The `md' prefix stands for `message digest'
+(define *gpg:hash-algorithm-enum*
+  '((0 . gpg:md-none)
+    (1 . gpg:md-md5)
+    (2 . gpg:md-sha1)
+    (3 . gpg:md-rmd160)
+    (5 . gpg:md-md2)
+    (6 . gpg:md-tiger)
+    (7 . gpg:md-haval)
+    (8 . gpg:md-sha256)
+    (9 . gpg:md-sha384)
+    (10 . gpg:md-sha512)
+    (301 . gpg:md-md4)
+    (302 . gpg:md-crc32)
+    (303 . gpg:md-crc32-rfc1510)
+    (304 . gpg:md-crc24-rfc2440)))
+
+
+;; The opposite of *gpg:hash-algorithm-enum*
+(define *gpg:hash->integer*
+  (map (lambda (p)
+	 (and (pair? p)
+	      (cons (cdr p) (car p))))
+       *gpg:hash-algorithm-enum*))
+
+
+;; String descriptions of the hash algorithms available
+(define *gpg:hash->name*
+  '((gpg:md-md5 . "MD5")
+    (gpg:md-sha1 . "SHA1")
+    (gpg:md-rmd160 . "RIPEMD160")
+    (gpg:md-md2 . "MD2")
+    (gpg:md-tiger . "TIGER192")
+    (gpg:md-haval . "HAVAL")
+    (gpg:md-sha256 . "SHA256")
+    (gpg:md-sha384 . "SHA384")
+    (gpg:md-sha512 . "SHA512")
+    (gpg:md-md4 . "MD4")
+    (gpg:md-crc32 . "CRC32")
+    (gpg:md-crc32-rfc1510 . "CRC32RFC1510")
+    (gpg:md-crc24-rfc2440 . "CRC24RFC2440")))
+
+
+;;; Hash algorithm accessor functions
+
+;; Translate hash algorithm -> string
+(define (gpg:hash-algo-name algo)
+  "\
+Returns a string describing the hash algorithm @var{algo}.  This
+string can be used to output the name of the hash algorithm to the
+user.
+
+If @var{algo} is not a recognized algorithm, returns @code{#f}."
+  (assq-ref *gpg:hash->name algo))
+
+
+;; Translate hash symbol -> integer (for interfacing to GPGME)
+(define (gpg:hash-algo->integer algo)
+  "\
+Translates the hash algorithm symbol @var{algo} to an integer; this
+function should only be used if interfacing to GPGME directly.
+
+If @var{algo} is not a recognized hash algorithm, returns @var{#f}."
+  (assq-ref *gpg:hash->integer* algo))
+
+
+;; Translate integer -> hash symbol (for interfacing to GPGME)
+(define (gpg:integer->hash-algo num)
+  "\
+Translates the integer @var{num} to its corresponding hash algorithm
+symbol; this function should only be used if interfacing to GPGME
+directly.
+
+If @var{num} is not a recognized value, returns @var{#f}."
+  (assq-ref *gpg:hash-algorithm-enum* num))
+
 ;;; algorithms.scm ends here
